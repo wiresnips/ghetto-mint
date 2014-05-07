43342e0dc21b990a3d76967b94bbf023fa0019a9
Ben <ben.ells@gmail.com>
2014-05-06 21:56:57 -0400

    cleaning up forgotten leftovers of a discarded refactor

--- init.rb
+++ init.rb
@@ -23,5 +23,4 @@ D = Mysql2::Client.new username: "root"
 D.query "create database if not exists finance;"
 D.query "use finance;"
 
-D.query "create table if not exists accounts( id int not null auto_increment, name varchar(255), balance decimal(10,2) not null default 0.00, primary key (id) );"
-D.query "create table if not exists transactions( account_id int, date date, label varchar(255), amount decimal(10, 2), foreign key (account_id) references accounts(id) );"
+D.query "create table if not exists transactions( account varchar(255), date date, label varchar(255), amount decimal(10, 2) );"
